47c6408ec3ff7439b633f3393007aa60838a4005
Blanccov <d.madej572@gmail.com>
2024-03-18 21:15:19 +0100

    init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Button from 'ui/Button'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>Essa</div>
+      <Button />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/ui/Button.tsx
@@ -0,0 +1,9 @@
+const Button = () => {
+    return (
+        <button>
+            Essaa
+        </button>
+    )
+}
+
+export default Button;
