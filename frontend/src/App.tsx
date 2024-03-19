026314b237260a3d266264a8436caa9f9ab7d7e2
Blanccov <d.madej572@gmail.com>
2024-03-19 13:59:47 +0100

    ResetPassword page -added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Login from "pages/Login/Login.tsx";
 import {HashRouter, Route, Routes} from "react-router-dom";
+import Reset from "pages/Login/Reset.tsx";
 
 function App() {
     return (
         <HashRouter>
             <Routes>
                 <Route path="/" element={<Login />}></Route>
+                <Route path="/reset" element={<Reset />}></Route>
             </Routes>
         </HashRouter>
     )
