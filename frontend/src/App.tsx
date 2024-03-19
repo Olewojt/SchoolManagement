6f4d378f4a3a6df9c214610cb5b6a5bbd84ee8f0
Blanccov <d.madej572@gmail.com>
2024-03-19 10:28:10 +0100

    Router -added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
 import Login from "pages/Login/Login.tsx";
+import {HashRouter, Route, Routes} from "react-router-dom";
 
 function App() {
     return (
-        <>
-          <Login />
-        </>
+        <HashRouter>
+            <Routes>
+                <Route path="/" element={<Login />}></Route>
+            </Routes>
+        </HashRouter>
     )
 }
 export default App
