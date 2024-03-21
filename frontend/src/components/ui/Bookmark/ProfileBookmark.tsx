545393994b201b99adabeb9798ece5aa0139a830
Blanccov <d.madej572@gmail.com>
2024-03-21 21:46:47 +0100

    Sidebar profilebookmark and other animations added

--- frontend/src/components/ui/Bookmark/ProfileBookmark.tsx
+++ frontend/src/components/ui/Bookmark/ProfileBookmark.tsx
@@ -2,17 +2,19 @@ import classes from "ui/Bookmark/Bookmark.module.scss";
 import {Link, LinkProps} from "react-router-dom";
 import SettingsIcon from "assets/icons/Icon.tsx";
 
-interface ProfileProps extends LinkProps{
+interface ProfileProps extends LinkProps {
     src: string
 }
 
 const ProfileBookmark = (props: ProfileProps) => {
     return (
-        <div>
-            <img src={props.src} />
-            <span>{props.children}</span>
-            <Link to={props.to} className={classes.bookmark__profile}>
-                <SettingsIcon />
+        <div className={classes.profile}>
+            <div className={classes.profile__data}>
+                <img src={props.src}/>
+                <span>{props.children}</span>
+            </div>
+            <Link to={props.to} className={classes.profile__icon}>
+                <SettingsIcon/>
             </Link>
         </div>
     )
