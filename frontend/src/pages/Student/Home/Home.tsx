28b7ac7b25c4d6869ec4a25b9b53bff7467d2001
fk122941 <fk122941@stud.ur.edu.pl>
2024-03-25 14:26:53 +0100

    renamed files, changed index from css -> scss, refactor code in: Login.scss, Home.tsx

--- frontend/src/pages/Student/Home/Home.tsx
+++ frontend/src/pages/Student/Home/Home.tsx
@@ -1,11 +1,8 @@
 import classes from "./Home.module.scss"
-import TaskCard from "ui/Card/TaskCard.tsx";
 
 const Home = () => {
-    const date = new Date();
     return(
         <main className={classes.home}>
-            <TaskCard title={"Rozprawka o miśce w bojlerze"} subject={"Bojlerowanie"} date={date} />
         </main>
     )
 }
