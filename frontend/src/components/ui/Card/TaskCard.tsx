f00db46e9a39a9373e9c2ae92278ea60d8b9ebc4
Blanccov <d.madej572@gmail.com>
2024-04-04 16:18:43 +0200

    TaskCard -open card with closing

--- frontend/src/components/ui/Card/TaskCard.tsx
+++ frontend/src/components/ui/Card/TaskCard.tsx
@@ -1,4 +1,4 @@
-import {DragEvent, MouseEventHandler} from "react";
+import {DragEvent, FormEvent, MouseEventHandler} from "react";
 import classes from "./TaskCard.module.scss";
 import {GroupIcon, PlusIcon} from "assets/icons/Icon.tsx";
 import DropIndicator from "layouts/TaskBoard/DropIndicator.tsx";
@@ -18,12 +18,17 @@ interface CardInterface {
 
 const TaskCard = (props: CardInterface) => {
     const formattedDate = new Date(props.date).toLocaleDateString(); // Formatowanie daty
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    }
+
     return (
         <>
             <DropIndicator beforeId={props.id} column={props.column}/>
             <div draggable="true"
                  onDragStart={(e) => props.handleDragStart(e, {title: props.title, id: props.id, column: props.column})}
-                 className={`${classes.card} ${classes[`card__${props.active}`]}`}>
+                 className={classes.card}>
                 <GroupIcon className={classes.card__icon}/>
                 <h1 className={classes.card__title}>{props.title}</h1>
                 <div className={classes.card__info}>
@@ -34,6 +39,21 @@ const TaskCard = (props: CardInterface) => {
                     <PlusIcon/>
                 </button>
             </div>
+            {props.active
+                && <>
+                    <div className={classes.background}></div>
+                    <div className={classes["open-card"]}>
+                        <div className={classes["open-card__container"]}>
+                            <div className={classes["open-card__blank"]}></div>
+                            <div className={classes["open-card__content"]}>
+                                <button className={classes["open-card__btn"]} type="button" onClick={props.onClick}>
+                                    <PlusIcon/>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </>
+            }
         </>
     );
 }
