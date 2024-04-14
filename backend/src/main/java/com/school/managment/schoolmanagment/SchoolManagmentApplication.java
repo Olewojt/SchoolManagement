980237a3e1745e43bfd9a35c57a6a3dc2bcb389d
fk122941 <fk122941@stud.ur.edu.pl>
2024-04-14 12:31:09 +0200

    init spring db module

---
+++ backend/src/main/java/com/school/managment/schoolmanagment/SchoolManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.school.managment.schoolmanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagmentApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/school/managment/schoolmanagment/SchoolManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.managment.schoolmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
