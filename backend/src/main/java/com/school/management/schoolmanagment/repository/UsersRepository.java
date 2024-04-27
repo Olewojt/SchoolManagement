67f242da8b3206e14f7cc17aa1c98c3b4983fcde
Krzychulec21 <krzychu123g@gmail.com>
2024-04-27 11:29:29 +0200

    create test for user and database connection

--- backend/src/main/java/com/school/management/schoolmanagment/repository/UsersRepository.java
+++ backend/src/main/java/com/school/management/schoolmanagment/repository/UsersRepository.java
@@ -4,7 +4,10 @@ import com.school.management.schoolmanagment.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsersRepository extends JpaRepository<Users, Long > {
-    Users findByEmail(String email);
+    Optional<Users> findByEmail(String email);
+    Boolean existsByEmailAndPassword(String email, String password);
 }
