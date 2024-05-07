f439480e4db5b9b5982a0ab41717f80a23c01e0d
Wojciech Olejko <wo122959@stud.ur.edu.pl>
2024-05-07 15:23:37 +0200

    ZIEL-41 Admin panel prototype

--- frontend/src/api/Teachers.tsx
+++ frontend/src/api/Teachers.tsx
@@ -1,93 +1,124 @@
 export interface DummyTeacher {
+    [key : string]: number | string | null;
+    id: number;
     name: string;
     surname: string;
     subject: string;
     class: string | null;
+    email: string;
 }
 
-export const DUMMY_TEACHERS: Array<DummyTeacher> = [
+export const DUMMY_TEACHERS: DummyTeacher[] = [
     {
+        id: 1,
         name: "John",
         surname: "Smith",
         subject: "Mathematics",
-        class: "5C"
+        class: "5C",
+        email: "JSmith@school.com"
     },
     {
+        id: 2,
         name: "Emily",
         surname: "Johnson",
         subject: "English",
-        class: "4B"
+        class: "4B",
+        email: "EJohnson@school.com"
     },
     {
+        id: 3,
         name: "Michael",
         surname: "Brown",
         subject: "Physics",
-        class: null
+        class: null,
+        email: "MBrown@school.com"
     },
     {
+        id: 4,
         name: "Sarah",
         surname: "Davis",
         subject: "History",
-        class: "2C"
+        class: "2C",
+        email: "SDavis@school.com"
     },
     {
+        id: 5,
         name: "David",
         surname: "Martinez",
         subject: "Physical Education",
-        class: "1A"
+        class: "1A",
+        email: "DMartinez@school.com"
     },
     {
+        id: 6,
         name: "Jennifer",
         surname: "Anderson",
         subject: "Art",
-        class: "6B"
+        class: "6B",
+        email: "JAnderson@school.com"
     },
     {
+        id: 7,
         name: "Robert",
         surname: "Wilson",
         subject: "Geography",
-        class: "2A"
+        class: "2A",
+        email: "RWilson@school.com"
     },
     {
+        id: 8,
         name: "Jessica",
         surname: "Taylor",
         subject: "Music",
-        class: "6A"
+        class: "6A",
+        email: "JTaylor@school.com"
     },
     {
+        id: 9,
         name: "Kevin",
         surname: "Thompson",
         subject: "Computer Science",
-        class: null
+        class: null,
+        email: "KThompson@school.com"
     },
     {
+        id: 10,
         name: "Laura",
         surname: "Garcia",
         subject: "Foreign Language",
-        class: "8A"
+        class: "8A",
+        email: "LGarcia@school.com"
     },
     {
+        id: 11,
         name: "Kevin",
         surname: "Thompson",
         subject: "Computer Science",
-        class: null
+        class: null,
+        email: "KThompson2@school.com"
     },
     {
+        id: 12,
         name: "Kevin",
         surname: "Thompson",
         subject: "Computer Science",
-        class: null
+        class: null,
+        email: "KThompson3@school.com"
     },
     {
+        id: 13,
         name: "Kevin",
         surname: "Thompson",
         subject: "Computer Science",
-        class: null
+        class: null,
+        email: "KThompson4@school.com"
     },
     {
+        id: 14,
         name: "Kevin",
         surname: "Thompson",
         subject: "Computer Science",
-        class: null
-    },
+        class: null,
+        email: "KThompson5@school.com"
+    }
 ];
--- frontend/src/components/ui/Management/UserManagement.tsx
+++ frontend/src/components/ui/Management/UserManagement.tsx
@@ -2,12 +2,32 @@ import classes from "./UserManagement.module.scss"
 import Search from "ui/Management/Search.tsx";
 import Button from "ui/Button/Button.tsx";
 import SelectOptions from "forms/SelectOptions.tsx";
-import {DUMMY_TEACHERS} from "api/Teachers.tsx";
+import {DUMMY_TEACHERS, DummyTeacher} from "api/Teachers.tsx";
+import {DUMMY_STUDENTS, DummyStudent} from "api/Students.tsx";
+import {SetStateAction, useState} from "react";
+import Input from "forms/Input.tsx";
 
 const UserManagement = () => {
 
-    const onSearchInput = () => {
-        // console.log("SEARCHING")
+    const [searchQuery, setSearchQuery] = useState('');
+    const [currentTab, setCurrentTab] = useState('TEACHERS'); // State to track current tab
+    const [editMode, setEditMode] = useState(false); // State to track edit mode
+    const [editedRow, setEditedRow] = useState(-1); // Track the currently edited row
+    const handleEditRow = (row: number) => {
+        setEditedRow(row);
+    }
+
+    const handleTabChange = (tabName: string) => {
+        setCurrentTab(tabName);
+    }
+
+    const editModeChange = (mode: boolean) => {
+        setEditedRow(-1);
+        setEditMode(mode);
+    }
+
+    const onSearchInput = (event: { target: { value: SetStateAction<string>; }; }) => {
+        setSearchQuery(event.target.value);
     }
 
     const DUMMY_SUBJECTS = [
@@ -16,11 +36,57 @@ const UserManagement = () => {
         {name: "Physics"},
     ]
 
+    interface TabConfig {
+        headers: string[];
+        data: DummyTeacher[] | DummyStudent[];
+    }
+
+    const tabConfig: { [key : string]: TabConfig } = {
+        TEACHERS: {
+            headers: ["Name", "Surname", "E-Mail", "Subject", "Class"],
+            data: DUMMY_TEACHERS
+        },
+        STUDENTS: {
+            headers: ["Name", "Surname", "E-mail", "Class", "PESEL", "Country", "City", "Street", "Home number", "Flat number"],
+            data: DUMMY_STUDENTS
+        },
+        PARENTS: {
+            headers: ["Name", "Surname", "E-mail", "Phone Number"],
+            data: []
+        }
+    };
+
+    const filteredData = tabConfig[currentTab].data.filter((item) => {
+        return (
+            item.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            item.surname.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            item.email.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+    });
+
     return (
         <div className={classes.content}>
             <div className={classes.filters}>
-                <Button className={classes.tab} type={"button"} children={"TEACHERS"}></Button>
-                <Button className={classes.tab} type={"button"} children={"STUDENTS"}></Button>
+                <Button
+                    className={`${classes.tab} ${currentTab === 'TEACHERS' ? classes.buttonOn : ''}`}
+                    type="button"
+                    onClick={() => handleTabChange('TEACHERS')}
+                    children={"TEACHERS"}
+                />
+                <Button
+                    className={`${classes.tab} ${currentTab === 'STUDENTS' ? classes.buttonOn : ''}`}
+                    type="button"
+                    onClick={() => handleTabChange('STUDENTS')}
+                    children={"STUDENTS"}
+                >
+                </Button>
+                <Button
+                    className={`${classes.tab} ${currentTab === 'PARENTS' ? classes.buttonOn : ''}`}
+                    type="button"
+                    onClick={() => handleTabChange('PARENTS')}
+                    children={"PARENTS"}
+                >
+                </Button>
             </div>
 
             <div className={classes.filters}>
@@ -31,21 +97,67 @@ const UserManagement = () => {
             <div className={classes.table_container}>
                 <table>
                     <thead>
-                        <tr>
-                            <th colSpan={4}>Name</th>
-                            <th colSpan={4}>Surname</th>
-                            <th colSpan={4}>Subject</th>
-                            <th colSpan={4}>Class</th>
-                        </tr>
+                        {currentTab && (
+                            <tr>
+                                {editMode && <th colSpan={2}>DELETE</th>}
+                                {tabConfig[currentTab].headers.map((header, index) => (
+                                    <th key={index} colSpan={4}>{header}</th>
+                                ))}
+                            </tr>
+                        )}
                     </thead>
 
                     <tbody>
-                        {DUMMY_TEACHERS.map((teacher, index) => (
-                            <tr key={index}>
-                                <td colSpan={4}>{teacher.name}</td>
-                                <td colSpan={4}>{teacher.surname}</td>
-                                <td colSpan={4}>{teacher.subject}</td>
-                                <td colSpan={4}>{teacher.class ? teacher.class : "-"}</td>
+                        {currentTab && filteredData.map((item, index) => (
+                            <tr key={index} id={item.id.toString()}>
+                                {editMode && (
+                                    <td colSpan={2}>
+                                        <Button type="button" className={classes.editButton} onClick={() => handleEditRow(item.id)} children="✏️"/>
+                                        <Button type="button" className={classes.editButton} children="❌"/>
+                                    </td>
+                                )}
+
+                                <td colSpan={4}>
+                                    {editMode && editedRow == item.id
+                                        ? <Input type={"text"} placeholder={item.name} onChange={() => {}}/>
+                                        : item.name}
+                                </td>
+                                <td colSpan={4}>
+                                    {editMode && editedRow == item.id
+                                        ? <Input type={"text"} placeholder={item.surname} onChange={() => {}}/>
+                                        : item.surname}
+                                </td>
+                                <td colSpan={4}>
+                                    {editMode && editedRow == item.id
+                                        ? <Input type={"text"} placeholder={item.email} onChange={() => {}}/>
+                                        : item.email
+                                    }
+                                </td>
+
+                                {currentTab === "TEACHERS" &&
+                                    <>
+                                        <td colSpan={4}>{item.subject}</td>
+                                        <td colSpan={4}>{item.class || "-"}</td>
+                                    </>
+                                }
+
+                                {currentTab === "STUDENTS" &&
+                                    <>
+                                        <td colSpan={4}>{item.class}</td>
+                                        <td colSpan={4}>{item.pesel}</td>
+                                        <td colSpan={4}>{item.country}</td>
+                                        <td colSpan={4}>{item.city}</td>
+                                        <td colSpan={4}>{item.street}</td>
+                                        <td colSpan={4}>{item.home_num || "-"}</td>
+                                        <td colSpan={4}>{item.flat_num || "-"}</td>
+                                    </>
+                                }
+
+                                {/*{currentTab === "PARENTS" &&*/}
+                                {/*    <>*/}
+                                {/*        */}
+                                {/*    </>*/}
+                                {/*}*/}
                             </tr>
                         ))}
                     </tbody>
@@ -53,7 +165,11 @@ const UserManagement = () => {
             </div>
 
             <div className={classes.filters}>
-                <Button type={"button"} children={"Edit"}></Button>
+                <Button
+                    className={editMode ? classes.buttonOn : ''}
+                    type={"button"} children={"Edit"}
+                    onClick={() => editModeChange(!editMode)}
+                />
                 <Button type={"button"} children={"Add"}></Button>
                 <Button type={"button"} children={"Save"}></Button>
                 <Button type={"button"} children={"Clear"}></Button>
