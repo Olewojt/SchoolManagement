aeded9bb649f485a5e68d9c9bc0ae537eacd1cf7
Tomasz Nowak <tn122958@stud.ur.edu.pl>
2024-05-14 18:13:11 +0200

    added task controller rest controller annotation

--- backend/src/main/java/com/school/management/schoolmanagment/controller/TaskController.java
+++ backend/src/main/java/com/school/management/schoolmanagment/controller/TaskController.java
@@ -3,14 +3,15 @@ package com.school.management.schoolmanagment.controller;
 import com.school.management.schoolmanagment.model.Task;
 import com.school.management.schoolmanagment.service.TaskService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/tasks")
 public class TaskController {
@@ -18,7 +19,7 @@ public class TaskController {
     private final TaskService taskService;
 
     @GetMapping("/assigned/{userId}")
-    public List<Task> findTasksAssignedToUser(@PathVariable Long userId) {
-        return taskService.findTasksAssignedToUser(userId);
+    public ResponseEntity<List<Task>> findTasksAssignedToUser(@PathVariable Long userId) {
+        return ResponseEntity.ok(taskService.findTasksAssignedToUser(userId));
     }
 }
